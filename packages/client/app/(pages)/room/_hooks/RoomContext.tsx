90a4f594c26c3100d4449cb8d9c50784328e5904
Aragon Joaquin <aragonjoaquin05@gmail.com>
2025-03-02 15:11:18 -0300

    :star: added dispatchs

--- packages/client/app/(pages)/room/_hooks/RoomContext.tsx
+++ packages/client/app/(pages)/room/_hooks/RoomContext.tsx
@@ -14,20 +14,44 @@ function useRoomReducer() {
 			payload
 		})
 
+	const AddMessage = (payload: PICK_PAYLOAD<'ADD_MESSAGE'>) =>
+		dispatch({
+			type: 'ADD_MESSAGE',
+			payload
+		})
+
+	const LeaveRoom = (payload: PICK_PAYLOAD<'LEAVE_ROOM'>) =>
+		dispatch({
+			type: 'LEAVE_ROOM',
+			payload
+		})
+
+	const DeleteMessage = (payload: PICK_PAYLOAD<'DELETED_MESSAGE'>) =>
+		dispatch({
+			type: 'DELETED_MESSAGE',
+			payload
+		})
+
 	return {
 		roomState,
-		AddRoom
+		AddRoom,
+		AddMessage,
+		LeaveRoom,
+		DeleteMessage
 	}
 }
 
 export function GetRoomContext({ children }: { children: ReactNode }) {
-	const { roomState, AddRoom } = useRoomReducer()
+	const { roomState, AddRoom, AddMessage, LeaveRoom, DeleteMessage } = useRoomReducer()
 
 	return (
 		<RoomContext.Provider
 			value={{
 				roomState,
-				AddRoom
+				AddRoom,
+				AddMessage,
+				LeaveRoom,
+				DeleteMessage
 			}}
 		>
 			{children}
--- packages/client/app/(pages)/room/_hooks/context.ts
+++ packages/client/app/(pages)/room/_hooks/context.ts
@@ -5,6 +5,9 @@ import { PICK_PAYLOAD, roomState } from '../_reducers/types'
 interface RoomContextProps {
 	roomState: roomState[]
 	AddRoom: (payload: PICK_PAYLOAD<'ADD_ROOM'>) => void
+	AddMessage: (payload: PICK_PAYLOAD<'ADD_MESSAGE'>) => void
+	LeaveRoom: (payload: PICK_PAYLOAD<'LEAVE_ROOM'>) => void
+	DeleteMessage: (payload: PICK_PAYLOAD<'DELETED_MESSAGE'>) => void
 }
 
 export const RoomContext = createContext<RoomContextProps>({} as RoomContextProps)
